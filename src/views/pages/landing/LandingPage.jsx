50d13ddafba0082d5946b5bcdb3bc9d3f1aba647
Manoj-ReactJs <manojd@devsdrop.studio>
2021-05-02 19:04:16 +0530

    semaintic tags

--- src/views/pages/landing/LandingPage.jsx
+++ src/views/pages/landing/LandingPage.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import "./styles.css";
 
 function LandingPage() {
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <React.Fragment>
+      <h2> HTML5 semantic tags </h2>
+          <header>
+          <nav>
+               <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="/about/">About</a></li>
+                    <li><a href="/blog/">Blog</a></li>
+               </ul>
+          </nav>
+         </header>
+
+         <section>
+               <h1> this my sections </h1>
+         </section>
+         
+         <footer>
+                  <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="/about/">About</a></li>
+                    <li><a href="/blog/">Blog</a></li>
+               </ul>
+         </footer>
+        
+    </React.Fragment>
   )
 }
 
