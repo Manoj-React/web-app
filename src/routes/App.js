0e0c4a6be4a33887a03df6df174f49d4b490035d
Manoj-ReactJs <manojd@devsdrop.studio>
2021-05-07 20:13:22 +0530

    sidebar add

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "../views/pages/landing/LandingPage";
-import Units from "../views/pages/landing/css/units/Unit.jsx";
+import Units from "../views/pages/landing/css/units/Unit";
+import Heading from "../views/pages/css/heading/Heading";
+import MiniDrawer from "../views/common/sideBar/SideBar";
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/Units" component={Units} />
-
+          <Route exact path="/Heading" component={Heading} />
+          <Route exact path="/MiniDrawer" component={MiniDrawer} />
         </Switch>
       </Router>
     </div>
